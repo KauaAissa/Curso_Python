417a40a800934b9dd0bc7d7715242fc88dcc97aa
KauaAissa <kaua.aissa.dev@gmail.com>
2025-01-23 15:56:43 -0300

    Day 1

--- arquivos/Section_3/aula12.py
+++ arquivos/Section_3/aula12.py
@@ -1,6 +1,6 @@
 nome = 'Kaua Aissa'
 altura = 1.92
 peso = 75
-imc = peso / (altura * altura)
+imc = peso / (altura ** 2)
 
 print(nome, 'tem', altura, 'de altura,', 'pesa', peso, 'quilos e seu IMC é', imc)
