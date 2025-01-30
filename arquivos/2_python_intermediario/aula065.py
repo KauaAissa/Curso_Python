9d821c3dd92457406423f2ceae8425e44a7e39c1
KauaAissa <kaua.aissa.dev@gmail.com>
2025-01-30 14:39:47 -0300

    Python Intermediario

---
+++ arquivos/2_python_intermediario/aula065.py
@@ -0,0 +1,16 @@
+# Funções
+
+# def imprimir(a, b, c):
+#     print(a, b, c)
+
+
+# imprimir(1,2,3)
+# imprimir(4,5,6)
+
+
+def saudacao(nome='sem nome'):
+    print(f'Ola, {nome}')
+
+saudacao("Kaua")
+saudacao("Luiz")
+saudacao()
