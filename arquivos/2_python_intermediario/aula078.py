cf3d7a4135435c803539be9a8abf0ef11fb77061
KauaAissa <kaua.aissa.dev@gmail.com>
2025-02-05 14:27:40 -0300

    Python Intermediario

--- arquivos/2_python_intermediario/aula078.py
+++ arquivos/2_python_intermediario/aula078.py
@@ -22,8 +22,22 @@ s1 = {'Luiz', 1, 2, 3}  # com dados
 # Métodos úteis:
 # add, update, clear, discard
 
+# s1 = set()
+# s1.add('Luiz')
+# s1.add(1)
+# s1.update(('Olá mundo', 1, 2 , 4, 5))
+# s1.clear()
+# s1.discard('Olá mundo')
+
 # Operadores úteis:
 # união | união (union) - Une
 # intersecção & (intersection) - Itens presentes em ambos
 # diferença - Itens presentes apenas no set da esquerda
 # diferença simétrica ^ - Itens que não estão em ambos
+
+s1 = {1, 2, 3}
+s2 = {2, 3, 4}
+s3 = s1 | s2
+s3 = s1 & s2
+s3 = s1 - s2
+s3 = s1 ^ s2
