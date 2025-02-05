fd3785d03a3cf62df5e729747546787a15ab680a
KauaAissa <kaua.aissa.dev@gmail.com>
2025-02-05 12:23:21 -0300

    Python intermediario

--- arquivos/2_python_intermediario/aula076.py
+++ arquivos/2_python_intermediario/aula076.py
@@ -1,12 +1,49 @@
-pessoa = {
-    'nome': 'kaua',
-    'sobrenome': 'aissa',
-    'idade': 19,
-    'altura': 1.92,
-    'endereco': [
-        {'rua': 'exemplo', 'numero': 928}
-    ]
-}
 
-print(pessoa)
-print(pessoa['nome'])
+# Dicionarios dict
+
+# pessoa = {
+#     'nome': 'kaua',
+#     'sobrenome': 'aissa',
+#     'idade': 19,
+#     'altura': 1.92,
+#     'endereco': [
+#         {'rua': 'exemplo', 'numero': 928}
+#     ]
+# }
+
+# print(pessoa)
+# print(pessoa['nome'])
+
+
+# len - quantas chaves
+# keys - iterável com as chaves
+# values - iterável com os valores
+# items - iterável com chaves e valores
+# setdefault - adiciona valor se a chave não existe
+# copy - retorna uma cópia rasa (shallow copy)
+# get - obtém uma chave
+# pop - Apaga um item com a chave especificada (del)
+# popitem - Apaga o último item adicionado
+# update - Atualiza um dicionário com outro
+p1 = {
+    'nome': 'Luiz',
+    'sobrenome': 'Miranda',
+}
+# print(p1['nome'])
+# print(p1.get('nome', 'Não existe'))
+
+# nome = p1.pop('nome')
+# print(nome)
+# print(p1)
+# ultima_chave = p1.popitem()
+# print(ultima_chave)
+# print(p1)
+# p1.update({
+#     'nome': 'novo valor',
+#     'idade': 30,
+# })
+# p1.update(nome='novo valor', idade=30)
+# tupla = (('nome', 'novo valor'), ('idade', 30))
+lista = [['nome', 'novo valor'], ['idade', 30]]
+p1.update(lista)
+print(p1)
