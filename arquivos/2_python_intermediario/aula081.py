e649f241c92e49e6da95a6dc206ff4fd4f857a2d
KauaAissa <kaua.aissa.dev@gmail.com>
2025-02-10 16:51:35 -0300

    Python Intermediario

---
+++ arquivos/2_python_intermediario/aula081.py
@@ -0,0 +1,16 @@
+lista = [
+    {'nome': 'Luiz', 'sobrenome': 'miranda'},
+    {'nome': 'Maria', 'sobrenome': 'Oliveira'},
+    {'nome': 'Daniel', 'sobrenome': 'Silva'},
+    {'nome': 'Eduardo', 'sobrenome': 'Moreira'},
+    {'nome': 'Aline', 'sobrenome': 'Souza'},
+]
+
+def exibir(lista):
+    for item in lista:
+        print(item)
+    print()
+
+lista_ordenada = lista.sort(key=lambda item: item['nome'])
+
+exibir()
